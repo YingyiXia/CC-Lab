436e6db1cfd3f660799f6b9bf1c9be1901e89258
YingyiXia <yx3730@nyu.edu>
2024-12-09 11:42:59 +0800

    Create gamePage.js

---
+++ The Struggle Campus/js/gamePage.js
@@ -0,0 +1,9 @@
+function setup() {
+  let canvas = createCanvas(1000,600);
+  canvas.parent('p5-canvas-container-game');
+  canvas.position(windowWidth / 2 - width / 2, windowHeight /2  - height / 2);
+}
+
+function draw(){
+  background(220)
+}
