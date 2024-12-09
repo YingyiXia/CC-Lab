4199b94d567502df0b45f8523ebddcd90c156dee
YingyiXia <yx3730@nyu.edu>
2024-12-09 16:10:28 +0800

    Update JobHunting.js

--- The Struggle Campus/js/JobHunting.js
+++ The Struggle Campus/js/JobHunting.js
@@ -21,7 +21,7 @@ function setup() {
   bug = new Bug();
 
   restartButton = createButton('Restart');
-  restartButton.position(width / 2 - 50, height / 2 + 100);
+  restartButton.position(width / 2 +300, height / 2 + 270);
   restartButton.size(100, 40);
   restartButton.mousePressed(restartGame);
   restartButton.hide();
